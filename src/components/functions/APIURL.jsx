025e68b2d71863e4f76ee8dc44c14114e6683567
Pablo Expósito <pabloexpops3@gmail.com>
2025-05-27 16:29:11 +0200

    Cambios en la url

--- src/components/functions/APIURL.jsx
+++ src/components/functions/APIURL.jsx
@@ -1,2 +1,4 @@
-const API_URL = "http://192.168.1.148:8000/api/"
+// const API_URL = "http://192.168.1.148:8000/api/"
+const API_URL = "http://adoring-shirley.82-165-173-179.plesk.page/api/";
+
 export default API_URL
